3ad1091af5e323f57f12b9c93612dbc27a99d9d3
Hassaan Yousafzai <yousafzaihassaan@gmail.com>
2025-06-08 05:47:34 +0500

    few tweaks in client.py

--- client/client.py
+++ client/client.py
@@ -3,7 +3,7 @@ from langchain_groq import ChatGroq
 def llm_client():
     llm = ChatGroq(
         model_name="llama-3.3-70b-versatile",
-        temperature=0.5,
+        temperature=0.8,
         max_tokens=1024
     )
         
