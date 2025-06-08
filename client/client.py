13112c22e92211d5af9a2696c8e012974436dc00
Hassaan Yousafzai <yousafzaihassaan@gmail.com>
2025-06-08 05:46:57 +0500

    client model changed

--- client/client.py
+++ client/client.py
@@ -2,7 +2,7 @@ from langchain_groq import ChatGroq
 
 def llm_client():
     llm = ChatGroq(
-        model_name="gemma2-9b-it",
+        model_name="llama-3.3-70b-versatile",
         temperature=0.5,
         max_tokens=1024
     )
